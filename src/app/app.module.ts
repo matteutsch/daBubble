9a447a4cee7d6d25ee3df54a8f40b1168b9ce90c
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-11-22 21:08:00 +0100

    firebase added
    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(
+      {
+        "projectId": "dabubble-a4d50",
+        "appId": "1:748766129558:web:35deae58792d029fe3c1b7",
+        "storageBucket": "dabubble-a4d50.appspot.com",
+        "apiKey": "AIzaSyDVMt0LFYltUGd0ecMh4__0KEy4sXltY4Q",
+        "authDomain": "dabubble-a4d50.firebaseapp.com",
+        "messagingSenderId": "748766129558"
+      }
+    )),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
