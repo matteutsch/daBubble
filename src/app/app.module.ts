656a5fa2520e40b2f14d7babc61b866065a8c6cf
matteutsch <matteutsch@gmail.com>
2023-11-22 21:49:07 +0100

    fix after merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dabubble-a4d50',
+        appId: '1:748766129558:web:35deae58792d029fe3c1b7',
+        storageBucket: 'dabubble-a4d50.appspot.com',
+        apiKey: 'AIzaSyDVMt0LFYltUGd0ecMh4__0KEy4sXltY4Q',
+        authDomain: 'dabubble-a4d50.firebaseapp.com',
+        messagingSenderId: '748766129558',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
