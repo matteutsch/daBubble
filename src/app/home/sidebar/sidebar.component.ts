037c031d52b474e4fc1546c5ee9fcabb9437b66f
matteutsch <matteutsch@gmail.com>
2023-12-12 14:27:14 +0100

    all users being shown in sidebar & scss fixes

--- src/app/home/sidebar/sidebar.component.ts
+++ src/app/home/sidebar/sidebar.component.ts
@@ -1,30 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCreateChannelComponent } from '../dialogs/dialog-create-channel/dialog-create-channel.component';
 import { ChatService } from 'src/app/home/shared/chat.service';
+import { User } from 'src/app/models/models';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnChanges {
+  @Input() currentUser!: User;
+  @Input() isMainChatChannel: any;
+
+  users: User[] = [];
+
   showChannels: boolean = true;
   showDirectMessages: boolean = true;
-  @Input() isMainChatChannel: any;
 
   constructor(
     public dialog: MatDialog,
-    public chatService: ChatService
-    ) {}
+    public chatService: ChatService,
+    public userService: UserService
+  ) {}
+
+  ngOnChanges() {
+    this.userService.getAllUsers().subscribe((users) => {
+      this.users = users.filter((user) => user.uid !== this.currentUser.uid);
+    });
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogCreateChannelComponent, {
-      panelClass: 'dialog-create-channel'
+      panelClass: 'dialog-create-channel',
       // data: {name: this.name, animal: this.animal},
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
       // this.animal = result;
     });
